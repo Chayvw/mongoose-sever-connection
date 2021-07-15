4733ec5a474834a186c2f6abdf42ac36dc3da9d8
Chay Williams <Chayvw@gmail.com>
2021-07-15 13:12:31 -0400

    inital sever set up

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+
+const PORT = process.env.PORT || 27017;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://chay618:test18@test18.ewohp.mongodb.net/test18?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  { useUnifiedTopology: true }
+});
+
+mongoose.connection.on("connected", ()=>{
+    console.log("connected")
+})
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
